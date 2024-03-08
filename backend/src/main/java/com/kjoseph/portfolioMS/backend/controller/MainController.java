516c3b8c03c965047adfa07a02a7e02bb89e7ece
Joseph <kevin.b.joseph@afs.com>
2024-03-08 17:21:37 -0600

    current username displayed on  home page, added postman for API verification

--- backend/src/main/java/com/kjoseph/portfolioMS/backend/controller/MainController.java
+++ backend/src/main/java/com/kjoseph/portfolioMS/backend/controller/MainController.java
@@ -1,5 +1,16 @@
 package com.kjoseph.portfolioMS.backend.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
 
+	@GetMapping("/")
+	public String getHomePage() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		return authentication.getName();
+	}
 }
